e9682edd9aa8a96b2f51dfac4513eded72982516
Murguii <muurguii10@gmail.com>
2025-05-19 16:17:33 +0200

    quitada opcion ver mapa en main menu

--- frontend/src/pages/MainMenuPage.tsx
+++ frontend/src/pages/MainMenuPage.tsx
@@ -104,14 +104,6 @@ const MainMenuPage: React.FC = () => {
           <Button 
             variant="primary" 
             size="large"
-            onClick={() => navigate('/map-view')}
-          >
-            Ver Mapa
-          </Button>
-          
-          <Button 
-            variant="primary" 
-            size="large"
           >
             Opciones
           </Button>
